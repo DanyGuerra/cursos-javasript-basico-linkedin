65adda7c8a2d9460b3664ff436f37af00f280408
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-03 17:43:37 -0500

    Operador this

--- 7 - Funciones o métodos/9 - Uso del operador this/js/app.js
+++ 7 - Funciones o métodos/9 - Uso del operador this/js/app.js
@@ -9,16 +9,15 @@ const boton = document.querySelector('.boton');
 //     console.log("Boton pulsado")
 // });
 
-boton.addEventListener('click', function () {
-    console.log(this);
-})
+// boton.addEventListener('click', function () {
+//     console.log(this);
+// })
 
 // boton.addEventListener('click', function () {
 //     console.log(this.innerHTML);
 // })
 
-// boton.addEventListener('click', () => {
-//     console.log(this)
-//     this.location = "http://www.google.com";
-
-// })
+boton.addEventListener('click', () => {
+    console.log(this)
+    this.location = "http://www.google.com";
+})
